39f6a97846cc8a7674c5e8bd72a2f088c32d1550
Chandra Sekhar Cherukuru <chandrasekharcherukuruu@gmail.com>
2025-11-09 07:53:39 +0530

    Fixed CORS to allow S3 frontend domain

--- NexFundBackend/src/main/java/com/chakri/fundly/config/SecurityConfig.java
+++ NexFundBackend/src/main/java/com/chakri/fundly/config/SecurityConfig.java
@@ -107,13 +107,18 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOriginPatterns(Arrays.asList(
+                "http://localhost:3000",
+                "http://nexfund-frontend-bucket.s3-website.ap-south-1.amazonaws.com"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 }
